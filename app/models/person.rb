dfb915c3d27c3d5e248d509d21d1ab7c7570f220
Alkattan <aqattan@yahoo.com>
2018-02-15 11:23:48 -0600

    5th update

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ApplicationRecord
-	has_many :addresses
-	has_one :profile
-	has_many :vehicles
+	has_many :addresses, :dependent => :destroy
+	has_one :profile, :dependent => :destroy
+	has_many :vehicles, :dependent => :destroy
 	validates_presence_of :name, :phone_number, :date_of_birth
 end
